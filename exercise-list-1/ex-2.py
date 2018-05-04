61f70bf885da4dded0aea2134990ea69b096b3dc
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2018-05-04 17:57:54 -0300

    feat: Add exercies 1 and 2 from list 1

--- exercise-list-1/ex-2.py
+++ exercise-list-1/ex-2.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+from sympy import symbols, latex
+from sympy.plotting import plot
+
+n = symbols('n')
+exp_0 = 8*(n*n)
+exp_1 = n*n*n
+p1 = plot(exp_0, exp_1, (n, 0, 13), legend=True, show=False)
+p1[1].line_color = 'r'
+p1[0].label = f'${latex(exp_0)}$'
+p1[1].label = f'${latex(exp_1)}$'
+p1.show()
